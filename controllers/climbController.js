c48f6daf9264c2f277b6f37327ab930128004aee
Mélina Hotte <memehottem@gmail.com>
2022-11-27 01:04:15 -0500

    Climb routes updated

--- controllers/climbController.js
+++ controllers/climbController.js
@@ -58,22 +58,75 @@ exports.create = async (req, res, next) => {
 };
 
 exports.getForUpdate = async (req, res, next) => {
+    try {
+        let result = await Climbs.findOne({
+            attributes: [],
+            include: {
+                model: Place,
+                attributes: ['user_id'],
+                required: true
+            },
+            where: {
+                title: req.params.title
+            }
+        });
 
+        console.log(result.place);
+        if (result === null) {
+            throwError(errors.climb.not_found, 'climb', 404, false);
+        } else if (result.place.userId !== req.user.id) {
+            throwError(errors.auth.unauthorized, 'climb_update', 403, false);
+        }
+
+        let climb = await Climbs.findOne({
+            attributes: ['title', 'description', 'style', 'difficulty_level'],
+            include: {
+                model: Place,
+                attributes: ['title'],
+                required: true
+            },
+            where: {
+                title: req.params.title
+            }
+        });
+
+        res.status(200).json({
+            code: successes.climb.update,
+            status: status.success,
+            result: {
+                title: climb.title,
+                description: climb.description,
+                style: climb.style,
+                difficultyLevel: climb.difficultyLevel,
+                placeTitle: climb.place.placeId
+            }
+        });
+    } catch (err) {
+        next(manageError(err, {
+            code: errors.routes.climb.update,
+            cause: 'climb_update'
+        }));
+    }
 };
 
 exports.update = async (req, res, next) => {
     try {
         let result = await Climbs.findOne({
             attributes: ['id', 'title'],
+            include: {
+                model: Place,
+                attributes: ['user_id'],
+                required: true
+            },
             where: {
-                placeId: req.body.placeId,
                 title: req.params.title
             }
         });
 
+        console.log(result.place);
         if (result === null) {
             throwError(errors.climb.not_found, 'climb', 404, false);
-        } else if (result.userId !== req.user.id) {
+        } else if (result.place.userId !== req.user.id) {
             throwError(errors.auth.unauthorized, 'climb_update', 403, false);
         } else if (req.body.title !== req.params.title) {
             throwError(errors.climb.unique_constraint, 'title', 422, false);
