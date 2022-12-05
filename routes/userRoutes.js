0f1636a033a598c09b6147ed6f05fe518ed05cda
Mélina <melina.hotte@gmail.com>
2022-12-05 09:18:08 -0500

    Fix auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+const { needsAuth } = require('../middlewares/is-auth');
+const { status } = require('../utils/enums');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-
+router.post('/auth', needsAuth, (req, res, next) =>
+    res.status(200).send({ status: status.success }));
 // Export des routes pour utilisation dans app.js
 module.exports = router;
