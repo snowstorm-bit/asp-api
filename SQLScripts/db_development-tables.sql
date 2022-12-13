7f806754d3b4d6779ddeb5f1b4a7fbc5f99a5289
Mélina <melina.hotte@gmail.com>
2022-12-13 01:22:24 -0500

    Adjust sql script for database and tables

--- SQLScripts/db_development-tables.sql
+++ SQLScripts/db_development-tables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Dec 02, 2022 at 05:31 PM
+-- Generation Time: Dec 13, 2022 at 06:18 AM
 -- Server version: 10.6.5-MariaDB
 -- PHP Version: 7.4.26
 
@@ -26,25 +26,21 @@ USE `db_development`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `climbs`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `climbs`;
-CREATE TABLE IF NOT EXISTS `climbs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `description` varchar(500) COLLATE latin1_general_ci NOT NULL,
-  `style` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `difficulty_level` decimal(3,2) NOT NULL,
-  `images` text COLLATE latin1_general_ci NOT NULL,
-  `place_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `place_id` (`place_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(50) COLLATE latin1_general_ci NOT NULL,
+    `email` varchar(50) COLLATE latin1_general_ci NOT NULL,
+    `password` varchar(255) COLLATE latin1_general_ci NOT NULL,
+    `access_level` int(1) NOT NULL,
+    `createdAt` datetime NOT NULL,
+    `updatedAt` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -66,26 +62,30 @@ CREATE TABLE IF NOT EXISTS `places` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `title` (`title`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `climbs`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `email` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `password` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `access_level` int(1) NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+DROP TABLE IF EXISTS `climbs`;
+CREATE TABLE IF NOT EXISTS `climbs` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` varchar(50) COLLATE latin1_general_ci NOT NULL,
+    `description` varchar(500) COLLATE latin1_general_ci NOT NULL,
+    `style` enum('traditional','sport','top_roping') COLLATE latin1_general_ci NOT NULL,
+    `difficulty_level` decimal(3,2) NOT NULL,
+    `images` text COLLATE latin1_general_ci NOT NULL,
+    `place_id` int(11) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `createdAt` datetime NOT NULL,
+    `updatedAt` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `place_id` (`place_id`),
+    KEY `user_id` (`user_id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -98,13 +98,15 @@ CREATE TABLE IF NOT EXISTS `user_rates` (
   `user_id` int(11) NOT NULL,
   `climb_id` int(11) NOT NULL,
   `rate` int(1) NOT NULL,
-  KEY `user_id` (`user_id`),
+  PRIMARY KEY (`user_id`,`climb_id`),
   KEY `climb_id` (`climb_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
--- Constraints for dumped tables
+-- Constraints for table `places`
 --
+ALTER TABLE `places`
+    ADD CONSTRAINT `places_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `climbs`
@@ -112,12 +114,6 @@ CREATE TABLE IF NOT EXISTS `user_rates` (
 ALTER TABLE `climbs`
   ADD CONSTRAINT `climbs_ibfk_1` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `climbs_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `places`
---
-ALTER TABLE `places`
-  ADD CONSTRAINT `places_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `user_rates`
